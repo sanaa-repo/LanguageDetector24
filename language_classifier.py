010dbaa1c8b34f82406e4d88f6f1168d8623d1ea
sanaa-repo <sanaakapur@gmail.com>
2024-04-29 14:58:39 -0700

    user input + trained model

--- language_classifier.py
+++ language_classifier.py
@@ -1,3 +1,28 @@
-from sklearn.naive_bayes import GaussianNB
+# Import statements
+from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
 
+# Load data from a CSV file
+train_data = pd.read_csv('data/train.csv')
+test_data = pd.read_csv('data/test.csv')
+
+X_train = train_data["text"]
+y_train = train_data["labels"]
+
+X_test = test_data["text"]
+y_test = test_data["labels"]
+
+vectorizer = CountVectorizer() #Found out about count vectorizer through ChatGPT
+X_train_counts = vectorizer.fit_transform(X_train)
+X_test_counts = vectorizer.transform(X_test)
+
+classifier = MultinomialNB()
+
+classifier.fit(X_train_counts, y_train)
+
+y_pred = classifier.predict(X_test_counts)
+
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy)
